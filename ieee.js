b7e8765c576a7d2371df8fa1044aaedf379fe38b
Raj82862 <77671289+Raj82862@users.noreply.github.com>
2021-06-27 17:18:29 +0530

    Add files via upload

---
+++ ieee.js
@@ -0,0 +1,25 @@
+function store(){
+  localStorage.setItem('r3',r3.value);
+  localStorage.setItem('r6',r6.value);
+}
+function ey(){
+  window.open("newr.html");
+}
+
+
+function validate() {
+  let username = document.getElementById('usernam').value;
+  let password = document.getElementById('passwor').value;
+  var newy = localStorage.getItem('r3');
+  var newset = localStorage.getItem('r6');
+
+    if(username == newy && password == newset) {
+        window.open("welcome.html");
+    }
+    else {
+        alert('Login Failed.');
+    }
+}
+function show(){
+  document.getElementById('q').innerHTML = 'Welcome,'+localStorage.getItem('r3');
+}
